fb06fe3e09c1005aa0905e41e5ee0190edb397ba
Oleksandr Hutsulyak <ohutsulyak@striketech.pl>
2015-09-06 22:04:56 +0200

    Added 'platforms' directory to gitignore

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/kamilius/proj/breastfeeding-diary/platforms/android/bin/AndroidManifest.xml : \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/breastfeedingdiary-debug-unaligned.apk.d
+++
@@ -1,8 +0,0 @@
-/home/kamilius/proj/breastfeeding-diary/platforms/android/bin/breastfeedingdiary-debug-unaligned.apk : \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/bin/breastfeedingdiary.ap_ \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/bin/classes.dex \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/libs/nativescript.jar \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/libs/android-support-v4.jar \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/libs/widgets.jar \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/libs/armeabi-v7a/libNativeScript.so \
-/home/kamilius/proj/breastfeeding-diary/platforms/android/libs/x86/libNativeScript.so \
